f9caf8fc0eb0312b92e82db044ffbcd97a0a70fd
xhachex <hriolfo@yahoo.com>
2018-09-25 15:51:08 -0300

    Update serial_comm.py

--- serial_comm.py
+++ serial_comm.py
@@ -1,2 +1,22 @@
-try:
-  from pyfi
+import serial
+from time import sleep
+
+
+
+ser = serial.Serial('COM3', baudrate = 9600, timeout = 1)
+sleep(3)
+
+def getValues():
+  ser.write(b'g')
+  arduinoData = ser.readline().decode('ascii')
+  return arduinoData
+
+
+while(1):
+  userInput = input('Get data poing?')
+  
+  if userInput == 'y':
+    print(getValues())
+ 
+
+  
