bc1e9e325293f845a0b29a2fe3f7c3a6e3d43faa
xhachex <hriolfo@yahoo.com>
2018-09-26 16:33:11 -0300

    Create gestos.py

---
+++ gestos.py
@@ -0,0 +1,28 @@
+from time import sleep
+
+def no()
+    print("Nop")
+    servo_pan(90)
+    sleep(0.1)
+    for x in range(3)
+        servo_pan(70)
+        sleep(0.1)
+        servo_pan(110)
+        sleep(0.1)
+    servo_pan(90)
+    sleep(0.1)
+    
+        
+def yes()
+    print("Nop")
+    servo_pan(90)
+    sleep(0.1)
+    for x in range(3)
+        servo_pan(70)
+        sleep(0.1)
+        servo_pan(110)
+        sleep(0.1)
+    servo_pan(90)
+    sleep(0.1)
+
+def happy
