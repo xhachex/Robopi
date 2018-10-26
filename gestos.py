6df8fc00812ce527b25386090295c7ed3356acf6
xhachex <hriolfo@yahoo.com>
2018-10-26 16:54:43 -0300

    Update gestos.py

--- gestos.py
+++ gestos.py
@@ -1,28 +1,20 @@
+import pigpio
 from time import sleep
 
-def no()
-    print("Nop")
-    servo_pan(90)
-    sleep(0.1)
-    for x in range(3)
-        servo_pan(70)
-        sleep(0.1)
-        servo_pan(110)
-        sleep(0.1)
-    servo_pan(90)
-    sleep(0.1)
-    
-        
-def yes()
-    print("Yes")
-    servo_tilt(90)
-    sleep(0.1)
-    for x in range(3)
-        servo_tilt(70)
-        sleep(0.1)
-        servo_tilt(110)
-        sleep(0.1)
-    servo_tilt(90)
-    sleep(0.1)
+pi = pigpio.pi()
 
-def happy
+def yep():
+    print("Yeai")
+    for x in range(3):
+        pi.set_servo_pulsewidth(3, 1000)
+        sleep(.3)
+        pi.set_servo_pulsewidth(3, 2000)
+        sleep(.3)            
+
+def nop():
+    print("No no")
+    for x in range(3):
+        pi.set_servo_pulsewidth(2, 1000)
+        sleep(.3)
+        pi.set_servo_pulsewidth(2, 2000)
+        sleep(.3)
