4dd6dd715a0945bb8d7619c16f8fd126285c8e34
xhachex <hriolfo@yahoo.com>
2019-01-16 07:41:42 -0300

    Update mov.py

--- mov.py
+++ mov.py
@@ -21,5 +21,15 @@ def left_open(speed):
 	left_wheel.stop()
 	right_wheel.forward(speed)
 
-def left_closed(speed)
-	left_wheel
+def left(speed)
+	left_wheel.backward(speed)
+	right_wheel.forward(speed)
+
+def right_open(speed):
+	right_wheel.stop()
+	left_wheel.forward(speed)
+
+def right(speed)
+	right_wheel.backward(speed)
+	left_wheel.forward(speed)
+	
